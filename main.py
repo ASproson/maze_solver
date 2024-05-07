3e40c8325b05b6198ad18edc524e2cf7f8a64b7c
ASproson <a.sproson@outlook.com>
2024-05-07 13:36:37 +0100

    Abstracts graphics

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-from tkinter import Tk, BOTH, Canvas
 
-class Window():
-  def __init__(self, width, height):
-    self.width = width
-    self.height = height
-    self.root = Tk()
-    self.root.title("Maze Solver")
-    self.canvas = Canvas(self.root, width = self.width, height = self.height)
-    self.canvas.pack()
-    self.running = False
+from graphics import Window
 
-  def redraw(self):
-    self.root.update_idletasks()
-    self.root.update()
+def main():
+  win = Window(800, 600)
+  win.wait_for_close()
+
+
+main()
