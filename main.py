228ac05ca6e80892649ae708024a8fcbed2bc9c6
ASproson <a.sproson@outlook.com>
2024-05-07 13:54:55 +0100

    Adds initial p-p line renders

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
   win = Window(800, 600)
+
+  # Create Points
+  point1 = Point(50, 50)
+  point2 = Point(200, 200)
+  point3 = Point(100, 300)
+
+  # Create Lines
+  line1 = Line(point1, point2)
+  line2 = Line(point2, point3)
+
+  # Draw Lines
+  win.draw_line(line1, "black")
+  win.draw_line(line2, "red")
+
   win.wait_for_close()
 
-main()
+if __name__ == "__main__":
+    main()
