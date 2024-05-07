4cf442f0d0bd3d1bf5e90d87d3e95eda5635b536
ASproson <a.sproson@outlook.com>
2024-05-07 15:26:50 +0100

    Adds logic to draw between cells

--- cell.py
+++ cell.py
@@ -30,3 +30,22 @@ class Cell():
       if self.has_bottom_wall:
           line = Line(Point(x1, y2), Point(x2, y2))
           self._win.draw_line(line)
+
+  def draw_move(self, to_cell, undo = False):
+    curr_x = (self._x1 + self._x2) / 2
+    curr_y = (self._y1 + self._y2) / 2
+
+    target_x = (to_cell._x1 + to_cell._x2) / 2
+    target_y = (to_cell._y1 + to_cell._y2) / 2
+
+    line = Line(Point(curr_x, curr_y), Point(target_x, target_y))
+
+    if undo is True:
+      self._win.draw_line(line, "red")
+    else:
+      self._win.draw_line(line, "gray")
+       
+
+
+
+    
