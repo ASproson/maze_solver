2707107846231186df487d98a4d537daedef9754
ASproson <a.sproson@outlook.com>
2024-05-08 14:06:51 +0100

    Adds visited prop

--- cell.py
+++ cell.py
@@ -11,13 +11,14 @@ class Cell():
     self._y1 = None
     self._y2 = None
     self._win = win
+    self.visited = False
 
   def draw(self, x1, y1, x2, y2):
       self._x1 = x1
       self._x2 = x2
       self._y1 = y1
       self._y2 = y2
-      
+
       wall_color = lambda has_wall: "black" if has_wall else "white"
       
       # Left wall
