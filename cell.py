3072d8050070794ae05c662ca0bf8ec0777fc348
ASproson <a.sproson@outlook.com>
2024-05-08 13:08:27 +0100

    Ensures removal of traversed lines

--- cell.py
+++ cell.py
@@ -1,6 +1,5 @@
 from graphics import Line, Point
 
-
 class Cell():
   def __init__(self, win = None):
     self.has_left_wall = True
@@ -21,15 +20,27 @@ class Cell():
       if self.has_left_wall:
           line = Line(Point(x1, y1), Point(x1, y2))
           self._win.draw_line(line)
+      else:
+          line = Line(Point(x1, y1), Point(x1, y2))
+          self._win.draw_line(line, "white")
       if self.has_top_wall:
           line = Line(Point(x1, y1), Point(x2, y1))
           self._win.draw_line(line)
+      else:
+          line = Line(Point(x1, y1), Point(x2, y1))
+          self._win.draw_line(line, "white")
       if self.has_right_wall:
           line = Line(Point(x2, y1), Point(x2, y2))
           self._win.draw_line(line)
+      else:
+          line = Line(Point(x2, y1), Point(x2, y2))
+          self._win.draw_line(line, "white")
       if self.has_bottom_wall:
           line = Line(Point(x1, y2), Point(x2, y2))
           self._win.draw_line(line)
+      else:
+          line = Line(Point(x1, y2), Point(x2, y2))
+          self._win.draw_line(line, "white")
 
   def draw_move(self, to_cell, undo = False):
     curr_x = (self._x1 + self._x2) / 2
@@ -44,9 +55,3 @@ class Cell():
 
     line = Line(Point(curr_x, curr_y), Point(target_x, target_y))
     self._win.draw_line(line, fill_color)
-
-       
-
-
-
-    
